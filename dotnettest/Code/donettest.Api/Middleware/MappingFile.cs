6f0b58f4d1ffe1083537561d2dfa57a035358758
abhilipsan-Hexaware <1000049191@hexaware.com>
2022-11-29 14:48:41 +0000

    1st commit.

---
+++ dotnettest/Code/donettest.Api/Middleware/MappingFile.cs
@@ -0,0 +1,11 @@
+using donettest.BusinessEntities.Entities;
+using donettest.Contracts.DTO;
+using AutoMapper;
+
+public class MappingFile : Profile
+{
+    public MappingFile()
+    {
+        // Mapping variables
+    }
+}
---
+++ dotnettest/Code/donettest.Data/Interfaces/ISave.cs
@@ -0,0 +1,7 @@
+namespace donettest.Data.Interfaces
+{
+    public interface ISave<in T> where T : class
+    {
+        bool Save(T entity);
+    }
+}
